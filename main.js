d9a98c231137326cf6cbf4091008fc8c1252f6b8
Sarah E <seatherly.prsvr@gmail.com>
2023-10-05 17:46:31 -0500

    working commit

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const tock = new Audio('sounds/tock.mp3');
 const kickdrum = new Audio('sounds/kick-drum.mp3');
 const snaredrum = new Audio('sounds/snare-drum.mp3');
 const hihat = new Audio('sounds/hi-hat.mp3');
+const eightBass = new Audio('489892__tkky__slutty-808.wav');
+
 
 let count = 0;
 // This function is called every 600ms
@@ -16,6 +18,15 @@ function update() {
     const kickdrumBox = document.querySelector('#kickdrum');
     const snaredrumBox = document.querySelector('#snaredrum');
     const hihatBox = document.querySelector('#hihat');
+    const kickBeat = document.querySelector('#kickNum');
+    const snareBeat = document.querySelector('#snareNum');
+    const snareBeat2 = document.querySelector('#snareNum2');
+    const hihatBeat = document.querySelector('#hihatNum');
+    const hihatBeat2 = document.querySelector('#hihatNum2');
+    const eightBassBox = document.querySelector('#eightBass');
+    const eightBassBeat = document.querySelector('#eightBassNum');
+    const kickBeat2 = document.querySelector('#kickNum2');
+
     // Play the 'tick' sound
     if(metronomeBox.checked){if(count % 4 !== 0){
         tick.play()
@@ -23,32 +34,91 @@ function update() {
         tock.play()
     }}
     if(kickdrumBox.checked){
-        kickdrum.play()
+        if(kickBeat.value == count){
+            kickdrum.play()
+        }
+        if(kickBeat2.value == count){
+            kickdrum.play()
+        }
     }
     if(snaredrumBox.checked){
-        snaredrum.play()
+        if(snareBeat.value == count){
+            snaredrum.play()
+        }
+        if(snareBeat2.value == count){
+            snaredrum.play()
+        }
     }
     if(hihatBox.checked){
-        hihat.play()
+        if(hihatBeat.value == count){
+            hihat.play()
+        }
+        if(hihatBeat2.value == count){
+            hihat.play()
+        }
+    }
+    if(eightBassBox.checked){
+        if(eightBassBeat.value == count){
+            eightBass.play()
+        }
+    
     }
 
-    const kickBeat = document.querySelector('#kickNum');
-    const snareBeat = document.querySelector('#snareNum');
-    const hihatBeat = document.querySelector('#hihatNum');
+    
     
     let counter = document.querySelector('p');
-    counter.innerText = count
+    counter.innerText = count;
     
 }
 
+let temposelection = document.querySelector('#tempos');
+
+temposelection.addEventListener('change', ()=>{
+    let arm = document.querySelector('.arm');
+    if(temposelection.value === 'fastest'){
+        clearInterval(intervalID)
+        setupUpdate(800)
+        arm.style.animation = 'tick 1600ms ease-in-out infinite';
+        
+    }
+    if(temposelection.value === 'faster'){
+        clearInterval(intervalID)
+        setupUpdate(900)
+        arm.style.animation = 'tick 1800ms ease-in-out infinite';
+        
+    }
+    if(temposelection.value === 'normal'){
+        clearInterval(intervalID)
+        setupUpdate(1000)
+        arm.style.animation = 'tick 2000ms ease-in-out infinite';
+        
+    }
+    if(temposelection.value === 'slower'){
+        clearInterval(intervalID)
+        setupUpdate(1100)
+        arm.style.animation = 'tick 2200ms ease-in-out infinite';
+        
+    }
+    if(temposelection.value === 'slowest'){
+        clearInterval(intervalID)
+        setupUpdate(1200)
+        arm.style.animation = 'tick 2400ms ease-in-out infinite';
+        
+    }
+})
 
 
 
+var intervalID;
 
 // This function sets up update() to be called every 600ms
-function setupUpdate() {
-    let intervalID = setInterval(update, 600);
+function setupUpdate(speed) {
+    intervalID = setInterval(update, speed);
 }
 
-// Call setupUpdate() once after 300ms
-setTimeout(setupUpdate, 300);
+
+
+
+
+setTimeout(setupUpdate(1000), 300);
+
